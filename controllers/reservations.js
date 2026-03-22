4f70c5545b39dbc9f5d4056b08c6b6b48e07b226
BNiDGH <benekrub@gmail.com>
2026-03-22 20:26:40 +0700

    getReservations return email

--- controllers/reservations.js
+++ controllers/reservations.js
@@ -1,41 +1,57 @@
 const Reservation = require('../models/Reservation');
 const Restaurant = require('../models/Restaurant');
 
-exports.getReservations=async (req,res,next)=>{
+exports.getReservations = async (req, res, next) => {
     let query;
-    //General users can see only their appointments!
-    if(req.user.role !== 'admin'){
-        query=Reservation.find({user:req.user.id}).populate({
-            path: 'restaurant',
-            select: 'name province tel'
-        });
-    }
-    else{   //If you are an admin, you can see all!
-        if(req.params.restaurantId){
-            console.log(req.params.restaurantId);
-            query=Reservation.find({restaurant: req.params.restaurantId}).populate({
-            path: 'restaurant',
-            select: 'name province tel'
-        });
-        }else{
-            query=Reservation.find().populate({
+    // General users can see only their appointments!
+    if (req.user.role !== 'admin') {
+        query = Reservation.find({ user: req.user.id })
+            .populate({
                 path: 'restaurant',
                 select: 'name province tel'
+            })
+            .populate({
+                path: 'user',
+                select: 'name email' 
             });
+    } else {   
+        // If you are an admin, you can see all!
+        if (req.params.restaurantId) {
+            console.log(req.params.restaurantId);
+            query = Reservation.find({ restaurant: req.params.restaurantId })
+                .populate({
+                    path: 'restaurant',
+                    select: 'name province tel'
+                })
+                .populate({
+                    path: 'user',
+                    select: 'name email'
+                });
+        } else {
+            query = Reservation.find()
+                .populate({
+                    path: 'restaurant',
+                    select: 'name province tel'
+                })
+                .populate({
+                    path: 'user',
+                    select: 'name email'
+                });
         }
-        
     }
     try {
-        const Reservations= await query;
-        res.status (200).json({
-            success:true,
+        const Reservations = await query;
+        res.status(200).json({
+            success: true,
             count: Reservations.length,
             data: Reservations
         });
     } catch (error) {
         console.log(error);
-        return res.status(500).json({success:false, message:
-        "Cannot find Reservation"});
+        return res.status(500).json({
+            success: false, 
+            message: "Cannot find Reservation"
+        });
     }
 };
 exports.getReservation=async (req,res,next) =>{
@@ -101,7 +117,7 @@ exports.updateReservation=async (req,res, next)=>{
             return res.status(404).json({success:false, message:` No reservation with the id of ${req.params.id}`});
         }
         //Make sure user is the reservation owner
-        if(reservation.user.toString()!== req.user.id && req.user.role !== 'admid'){
+        if(reservation.user.toString()!== req.user.id && req.user.role !== 'admin'){
             return res.status(401).json({success:false,message:`User ${req.user.id} is not authorized to update this reservation`});
         }
 
